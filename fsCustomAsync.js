dca721acc8ea92e3d30f82809643347311e2e067
jaredscheib <jared.scheib@gmail.com>
2016-02-06 02:02:27 -0800

    (feat) Implement concatImgDataAsync

---
+++ fsCustomAsync.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
+const readPath = './fixtures/';
+
+var concatImgDataAsync = (dirPath) => {
+  let allImgFiles = [];
+  let allImgData = {};
+
+  return Promise.all(fs.readdirAsync(readPath)
+    .then((data) => {
+      return data
+      .filter((file) => { return file.indexOf('all_data') !== -1; })
+      .map((file) => {
+        allImgFiles.push(file);
+        return fs.readFileAsync(`./fixtures/${file}`, 'utf8');
+      });
+    }))
+    .then((imgSets) => {
+      imgSets.forEach((set, i) => {
+        let key = allImgFiles[i].split('-')[0];
+        allImgData[key] = JSON.parse(set);
+      });
+      return fs.writeFileAsync('./fixtures/allImgData.js', `var allImgData = ${JSON.stringify(allImgData, null, 4)};`, { flags: 'w' })
+    })
+    .catch((err) => { throw err; })  
+};
+
+concatImgDataAsync(readPath)
+.then(() => {
+  console.log('success');
+})
+.catch((err) => {
+  console.log('error', err);
+  throw err;
+});
