19a0cab674956865651e8aa7eb627f4bdc3f9d8b
jaredscheib <jared.scheib@gmail.com>
2016-02-07 21:23:58 -0800

    Promisify and add img_sanitizer_task refs

--- db-helpers.js
+++ db-helpers.js
@@ -1,11 +1,12 @@
 'use strict';
 
-// const Promise = require('bluebird');
+const Promise = require('bluebird');
 const fs = require('fs');
 const Firebase = require('firebase');
 
 const dbRoot = new Firebase('https://dazzling-heat-3394.firebaseio.com/');
 const imgRefRef = new Firebase('https://dazzling-heat-3394.firebaseio.com/img_ref');
+const imgSanitizerTaskRef = new Firebase('https://dazzling-heat-3394.firebaseio.com/img_sanitizer_task');
 
 var allImgData;
 var allImgDataUpdated = [];
@@ -38,29 +39,42 @@ var flattenImgSets = (readPath, writePath) => {
 
 // flattenImgSets('./www/allImgData.json', './www/allImgData-flat.json');
 
-var pushToFirebaseAndAddUIDs = (data) => {
-  data.forEach((imgData) => {
-    let item = imgData;
-    var tempRef = imgRefRef.push(item, (err) => {
-      if (err) throw err;
-    });
-    item.uid = tempRef.path.u[1];
-    console.log(item);
-    tempRef.update({uid: item.uid}, (err) => {
-      console.log(`item at ${item.uid} updated`);
-    });
-  });
-  // imgRefRef.set(data);
+var pushAndAddUID = (targetRef, sourceObj) => {
+  let item = sourceObj;
+  targetRef.push(item)
+    .then((tempRef) => {
+      item.uid = tempRef.path.u[1];
+      return tempRef.update({uid: item.uid});
+    })
+    .then(() => {
+      console.log(`item at ${targetRef.path.u[0]}/${item.uid} updated`);
+    })
+    .catch((err) => { throw (err); });
 };
 
-var pushImgSetsWithUIDsToFirebase = (readPath) => {
-  fs.readFile(readPath, 'utf-8', (err, imgSets) => {
-    imgSets = JSON.parse(imgSets);
-    return pushToFirebaseAndAddUIDs(imgSets);
+// fs.readFile(readPath, 'utf-8', (err, imgSet) => {
+//   imgSet = JSON.parse(imgSet);
+//   pushAndAddUIDs(imgRefRef, imgSet);
+// });
+
+var pushAndAddUIDs = (targetRef, sourceSet) => {
+  sourceSet.forEach((item) => {
+    pushAndAddUID(imgRefRef, item);
   });
 };
 
-// pushImgSetsWithUIDsToFirebase(filePathFlat);
-
-
+imgRefRef.once('value', (snapshot) => {
+  var data = snapshot.val();
+  var allPromises = [];
+  for (var key in data) {
+    var imgSanitizerTask = {
+      img_ref_uid: key
+    }
+    allPromises.push(pushAndAddUID(imgSanitizerTaskRef, imgSanitizerTask));
+  }
+  Promise.all(allPromises)
+    .then(() => {
+      console.log('all img_sanitizer_task refs added');
+    })
+});
 
